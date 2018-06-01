4ef21499e5d050e330bf29f2ee16968f255044a6
Guilherme Fickel <guilhermefickel@gmail.com>
2018-05-31 22:36:18 -0300

    Adding a History of commands (not saving to a file yet), making the interface ready to select multiple HTTP verbs and multiple arguments with different types. Also including vector and thread made the compilation time go from 600ms to over 1000ms :(

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <thread>
 #include <atomic>
 #include <stdio.h>
+#include <vector>
 #include "json/json.h"
 #include "imgui.h"
 #include "imgui_impl_glfw_gl3.h"
@@ -8,13 +9,45 @@
 #include <GLFW/glfw3.h>
 #include <curl/curl.h>
 
-// typedef struct History {
-//     ImGui::ImVector<std::string> urls;
-//     ImGui::ImVector<ImVector<std::string>> params_name;
-//     ImGui::ImVector<ImVector<std::string>> params_val;
-//     ImGui::ImVector<std::string> results;
-//     ImGui::ImVector<int> response_codes;
-// } History;
+
+typedef struct Buffer {
+    char buf[4098];
+} Buffer;
+
+typedef struct Argument { 
+    std::string name;
+    void* value_ptr;
+    int arg_type; // TODO: transform this to an ENUM soon!!!!
+} Argument; 
+
+
+typedef struct History {
+    std::string url;
+    std::vector<Argument> args;
+    std::string result;
+    int response_code;
+} History;
+
+
+
+
+void removeHistory(std::vector<History>& history, int idx) {
+    for (int i=0; i<(int)history[idx].args.size(); i++) {
+        free(history[idx].args[i].value_ptr); 
+    }
+    history.erase(history.begin() + idx);
+}
+
+
+template <typename T>
+Argument createArgument(std::string name, const T& val, int argType) {
+    Argument arg;
+    arg.name = name;
+    arg.value_ptr = (void*)malloc(sizeof(T));
+    arg.arg_type = argType;
+
+    return arg;
+}
 
 
 typedef struct MemoryStruct {
@@ -43,7 +76,7 @@ WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 }
 
 
-void threadRequestGet(std::atomic<bool>& request_done, CURL* curl, CURLcode& res, const char* buf, std::string& thread_result) { 
+void threadRequestGet(std::atomic<bool>& request_done, CURL* curl, CURLcode& res, const char* buf, std::string& thread_result, int& response_code) { 
     MemoryStruct chunk;
     curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
     curl_easy_setopt(curl, CURLOPT_URL, buf);
@@ -55,6 +88,9 @@ void threadRequestGet(std::atomic<bool>& request_done, CURL* curl, CURLcode& res
     //curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
     res = curl_easy_perform(curl);
     std::string response_body;
+    long resp_code;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &resp_code);
+    response_code = (int)resp_code;
     if(res != CURLE_OK) {
         thread_result = std::string(curl_easy_strerror(res));
         if(chunk.size > 0) thread_result = std::string(chunk.memory); 
@@ -99,7 +135,20 @@ int main(int argc, char* argv[])
     CURLcode res;
     std::atomic<bool> request_done(false);
     std::string thread_result;
+    int response_code;
     std::thread thread;
+
+    // Such ugly code... Can I do something like this?
+    // char** arg_types[] = { {"Text", "File"}, "Text" }; 
+    std::vector<const char**> arg_types;
+    // POST
+    const char* post_types[] = {"Text", "File"};
+    // GET
+    const char* get_types[] = {"Text"};
+    arg_types.push_back(post_types);
+    arg_types.push_back(get_types);
+
+    std::vector<History> history;
 
     /* In windows, this will init the winsock stuff */ 
     curl_global_init(CURL_GLOBAL_ALL);
@@ -113,9 +162,16 @@ int main(int argc, char* argv[])
 
         {
             ImGui::Begin("Postgirl");//, NULL, ImGuiWindowFlags_NoMove);
+
+            const char* items[] = {"GET", "POST"};
+            static int request_type = 0;
+            ImGui::PushItemWidth(ImGui::GetContentRegionAvailWidth()*0.2);
+            ImGui::Combo("", &request_type, items, IM_ARRAYSIZE(items));
+            ImGui::SameLine();
+
             static char buf[4098] = "";
             static bool process_request = false;
-            ImGui::PushItemWidth(500);
+            ImGui::PushItemWidth(ImGui::GetContentRegionAvailWidth());
             if (ImGui::InputText("URL", buf, IM_ARRAYSIZE(buf), ImGuiInputTextFlags_EnterReturnsTrue) ) {
                 ImGui::SetKeyboardFocusHere(-1); // Auto focus previous widget
                 process_request = true; 
@@ -125,20 +181,57 @@ int main(int argc, char* argv[])
             if (process_request) {
                 if (curl) {
                     result = thread_result = std::string("Processing...");
-                    thread = std::thread(threadRequestGet, std::ref(request_done), curl, std::ref(res), buf, std::ref(thread_result));
+                    History hist;
+                    hist.url = std::string(buf);
+                    hist.result = std::string("");
+                    //hist.args =
+                    history.push_back(hist);
+                    switch(request_type) { 
+                        case 0: 
+                            thread = std::thread(threadRequestGet, std::ref(request_done), curl, std::ref(res), buf, std::ref(thread_result), std::ref(response_code));
+                            break;
+                        case 1:
+                            thread = std::thread(threadRequestGet, std::ref(request_done), curl, std::ref(res), buf, std::ref(thread_result), std::ref(response_code));
+                            break;
+                        default:
+                            result = thread_result = std::string("Invalid request type selected!");
+                            request_done = true;
+                    }
                 }
              
                 process_request = false;
+            }
+
+            static std::vector<Argument> curr_args;
+            static std::vector<Buffer> args_buffers;
+
+
+            if (curr_args.size() == 0) {
+                Argument arg = createArgument(std::string("TestArgument"), std::string("Test"), 0);
+                curr_args.push_back(arg);
+                Buffer b;
+                b.buf[0] = '\0';
+                args_buffers.push_back(b);
+            }
+
+            for (int i=0; i<(int)curr_args.size(); i++) {
+                ImGui::PushItemWidth(ImGui::GetContentRegionAvailWidth()*0.2);
+                ImGui::Combo("", &curr_args[i].arg_type, arg_types[request_type], IM_ARRAYSIZE(arg_types[request_type])); 
+                ImGui::SameLine();
+                ImGui::PushItemWidth(ImGui::GetContentRegionAvailWidth());
+                ImGui::InputText("Argument", args_buffers[i].buf, IM_ARRAYSIZE(args_buffers[i].buf));
             }
 
             ImGui::Text("Result");
             ImGui::PushTextWrapPos(ImGui::GetCursorPos().x + 500);
             if (request_done) {
                 result = thread_result;
+                history.back().result = result;
+                history.back().response_code = response_code;
                 thread.join();
                 request_done = false;
             }
-            ImGui::Text("%s",result.c_str());
+            ImGui::InputTextMultiline("##source", &result[0], result.size(), ImVec2(-1.0f, ImGui::GetContentRegionAvail()[1]), ImGuiInputTextFlags_AllowTabInput | ImGuiInputTextFlags_ReadOnly);
             ImGui::PopTextWrapPos();
 
             ImGui::End();
